11f66633daccb5af9e40db50b5fa9ca2c3c1d23c
Vu Nguyen <nxvu699134@gmail.com>
2023-06-01 15:40:20 +0700

    (testing): Config vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
     // ...
+    root: "./tests",
+    alias: {
+      "@src": path.resolve() + "/src",
+    },
+    silent: true,
   },
 });
